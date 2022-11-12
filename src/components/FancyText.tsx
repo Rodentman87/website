198e60da4b690c3ca55adadb775e0f8bc32ffeb5
Maisy <rodentman87@gmail.com>
2022-11-12 05:10:07 -0600

    Small changes to the fancy text

--- src/components/FancyText.tsx
+++ src/components/FancyText.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export const FancyText: React.FC<{ children: string }> = ({ children }) => {
 	return (
-		<span className="bg-stone-100 px-1 rounded-md hover:scale-110 transition-transform inline-block">
+		<span className="bg-white px-1 rounded-md hover:scale-110 transition-transform inline-block">
 			<span className="from-purple-500 to-orange-400 bg-gradient-to-r bg-clip-text text-transparent font-bold">
 				{children}
 			</span>
