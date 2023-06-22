294523d2a8beecc184b743125fbcd6517038ba68
Maisy <rodentman87@gmail.com>
2023-06-22 01:16:22 -0500

    fix naming of function

--- src/hooks/useFancyEffect.ts
+++ src/hooks/useFancyEffect.ts
@@ -6,10 +6,10 @@ export function useFancyEffects() {
 		window.localStorage.getItem("fancyEffects") === "true";
 	const [fancyEffects, setFancyEffects] = useState<boolean>(initialValue);
 
-	const toggleFancyEffects = (show: boolean) => {
+	const setShowFancyEffects = (show: boolean) => {
 		window.localStorage.setItem("fancyEffects", show.toString());
 		setFancyEffects(show);
 	};
 
-	return [fancyEffects, toggleFancyEffects] as const;
+	return [fancyEffects, setShowFancyEffects] as const;
 }
